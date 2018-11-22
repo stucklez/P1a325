c45b9acded0e3aad688c9dda4b4db497edc71c87
Alexander Stückler <alexander.stuckler@gmail.com>
2018-11-22 16:43:01 +0100

    Uden edits

--- delivery2.c
+++ delivery2.c
@@ -8,9 +8,7 @@ struct book {
   int day, month, year;
   char adresse[MAX_ADRESSE]
   int vejnummer;
- yeeetttteeeeet
-big yeet my negro
-morten han har en lille tissemand og han er ikke 186
+
   char title[TITLE_MAX], author[AUTHOR_MAX], publisher[PUBLISHER_MAX];
   int publishing_year;
   int university_text_book;
